79d10947511ef64eb0c2476ec038126517c08d1b
Meito Matsumoto <88807662+mei10-0403@users.noreply.github.com>
2022-03-19 00:50:32 +0900

    ホーム画面の作成 (#11)

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+import { userData } from "../Chart/userdata";
+import Gauge from "../Chart/Gauge";
+import UserActivity from "../Chart/Chart";
+import Data from "../Chart/Data";
+import { Container } from "react-bootstrap";
+import { Box, Grid, Typography } from "@mui/material";
+import "../../stylesheets/home.css";
 
-class Home extends React.Component{
-	render(){
-		return(
-			<div className='mainText'>Hello HOME</div>
-		);
-	}
+class Home extends React.Component {
+  render() {
+    return (
+      <>
+			<div class="date">
+				3月19日(土)
+			</div>
+        <Container fluid>
+          <Grid container spacing={1}>
+            <Grid item  md={5}>
+              <Gauge
+                data={userData}
+                title="一週間の学習記録"
+                grid
+                dataKey="Active User"
+              />
+							<UserActivity />
+            </Grid>
+            <Grid item xs={7} >
+              <Data />
+            </Grid>
+          </Grid>
+        </Container>
+      </>
+    );
+  }
 }
 
 export default Home;
