d938e56b129ceae5990242174a9c61c0900b5b4c
mei10-0403 <meito4319@gmail.com>
2022-03-19 11:49:03 +0900

    headerの位置

--- src/components/pages/TimerPage.js
+++ src/components/pages/TimerPage.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import MyStopwatch from '../Timer/Timer';
-import '../../stylesheets/timer.css';
+import React from "react";
+import MyStopwatch from "../Timer/Timer";
+import Header from "../modules/Header";
+import "../../stylesheets/timer.css";
 
-class Timer extends React.Component{
-	render(){
-		return(
-			<div className="TimerPage">
-				<h1 className='Timer'>LEARNING TIMER</h1>
-				<MyStopwatch />
-			</div>
-		);
-	}
+class Timer extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <div className="TimerPage">
+          <h1 className="Timer">LEARNING TIMER</h1>
+          <MyStopwatch />
+        </div>
+      </>
+    );
+  }
 }
 
 export default Timer;
