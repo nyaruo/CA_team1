08dd270999bc26d58519a8c3b47091fe6fc05428
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-03-19 13:17:34 +0900

    学習言語の登録フォーム追加

--- src/components/pages/TimerPage.js
+++ src/components/pages/TimerPage.js
@@ -2,15 +2,16 @@ import React from 'react';
 import MyStopwatch from '../Timer/Timer';
 import '../../stylesheets/timer.css';
 
-class Timer extends React.Component{
-	render(){
-		return(
-			<div className="TimerPage">
-				<h1 className='Timer'>LEARNING TIMER</h1>
-				<MyStopwatch />
-			</div>
-		);
-	}
+function Record() {
+	return (
+		<div className="RecordPage">
+			<section>
+ 				<h1 className='Timer'>LEARNING TIMER</h1>
+ 				<MyStopwatch />
+			</section>
+ 		</div>
+	)
 }
 
-export default Timer;
+
+export default Record;
